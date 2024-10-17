34be1feec92393e88a879a97d662a8301610a892
Fellipe Souto <fllsouto@gmail.com>
2024-10-16 22:20:38 -0300

    wk7: creating project and writting first API's

---
+++ checkpoint_2/libunclealegnment/src/main/java/br/com/fiap/twoespwx/libunclealegnment/LibunclealegnmentApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.twoespwx.libunclealegnment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibunclealegnmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibunclealegnmentApplication.class, args);
+	}
+
+}
