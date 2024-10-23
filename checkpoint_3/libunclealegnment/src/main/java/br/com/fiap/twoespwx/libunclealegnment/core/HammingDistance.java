3497d816051fa2d4122f909bf3670824c29c9440
Fellipe Souto <fllsouto@gmail.com>
2024-10-23 19:37:41 -0300

    wk8: creating abstract class and factory

--- checkpoint_3/libunclealegnment/src/main/java/br/com/fiap/twoespwx/libunclealegnment/core/HammingDistance.java
+++ checkpoint_3/libunclealegnment/src/main/java/br/com/fiap/twoespwx/libunclealegnment/core/HammingDistance.java
@@ -2,15 +2,34 @@ package br.com.fiap.twoespwx.libunclealegnment.core;
 
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+
 import br.com.fiap.twoespwx.libunclealegnment.api.input.DistanceInput;
 import br.com.fiap.twoespwx.libunclealegnment.api.input.SequenceInput;
-public class HammingDistance implements DistanceCalculator {
+
+@Component
+public class HammingDistance extends BaseDistance {
+
+    private static final String METHOD = "HAMMING_DISTANCE";
+    private static final String ABOUT = """
+        The Hamming Distance measures the difference between two strings of equal length by counting the number of positions where the corresponding characters differ. It is commonly used in error detection, bioinformatics, cryptography, and machine learning to assess similarity or detect changes. For example, the Hamming Distance between "ACGT" and "ACGG" is 1, as only the last character differs."
+    """;
+
+    private static final List<String> REFERENCES = List.of(
+        "https://www.sciencedirect.com/topics/computer-science/hamming-distance",
+        "https://en.wikipedia.org/wiki/Hamming_distance"
+    );
+
+    public HammingDistance() {
+        super(HammingDistance.METHOD, HammingDistance.ABOUT, HammingDistance.REFERENCES);
+    }
 
     @Override
     public DistanceResult run(DistanceInput input) {
         List<SequenceInput> sequences = input.getSequences();
         // calcular a similaridade usando o meu DistanceInput
         // preparar os dados de saída dentro do DistanceResult
+        // criar um builder para essa classe
         return null;
     }
     
---
+++ checkpoint_3/libunclealegnment/src/main/java/br/com/fiap/twoespwx/libunclealegnment/factory/DistanceCalculatorFactory.java
@@ -0,0 +1,11 @@
+package br.com.fiap.twoespwx.libunclealegnment.factory;
+
+import br.com.fiap.twoespwx.libunclealegnment.core.DistanceCalculator;
+
+public class DistanceCalculatorFactory {
+
+    public DistanceCalculator getInstance(String method) {
+        return null;
+    }
+    
+}
